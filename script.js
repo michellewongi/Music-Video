f7eb6df8c7f8aeb64c82c55e262031dd42b250e3
mw3981 <mw3981@nyu.edu>
2021-04-25 00:32:42 -0400

    second push

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
-// let fade;
-// let fadeAmnt = 1;
 let button;
-let ply;
-let rain;
+let hw;
+let stars;
 let song;
+let rain;
+let girl;
+let heart;
+let rose;
 
 function preload() {
-  song = loadSound('ladidie.mp3'); //loads mp3 file
-}
+  //song
+  song = loadSound('ladidie.mp3');
+
+  //backgrounds
+  hw = loadImage('images/hollywood.png');
+  stars = loadImage('images/stars.png');
+
+  //animations
+  rain = loadAnimation('images/rain_1.png', 'images/rain_2.png');
+  girl = loadAnimation('images/girl_1.png', 'images/girl_5.png');
+  heart = loadAnimation('images/heart_2.png', 'images/heart_13.png');
+  rose = loadAnimation('images/rose_1.png', 'images/rose_12.png');
+
+} //end of preload
 
 
 function setup() {
-  rain = new Rain();
   createCanvas(windowWidth, windowHeight);
 
-  ply = song.isPlaying();
+  //slows the speed of animation
+  rain.frameDelay = 10;
+  girl.frameDelay = 15;
+  heart.frameDelay = 13;
+  rose.frameDelay = 13;
 
   //creates a button
+  let col = color(188, 143, 143);
   button = createButton('Play');
-  button.position(windowWidth / 2, windowHeight / 2);
+  button.size(200, 100);
+  button.style("font-family", "Knewave");
+  button.style("font-size", "48px");
+  button.style("background-color", col);
+  button.position(windowWidth / 2 - 80, windowHeight / 2 - 50);
   button.mousePressed(start);
-  // fade = 255;
-}
+
+} //end of setup
+
 
 //starts the song and hides the button after clicked
 function start() {
   button.hide();
   song.setVolume(0.2);
   song.play();
-  song.stop([55]);
-}
+  song.stop([24]);
+} //end of start
 
 
 function draw() {
   background(0);
-  // background(0, 0, 0, fade);
-  // fill(255, 255, 255, fade);
-  // textSize(80);
-  // textAlign(CENTER, CENTER);
-  // text("La Di Die", width / 2, height / 2 - 30);
-  // text("Nessa Barret", width / 2, height / 2 + 80);
-  // if (fade >= 255) fadeAmnt = -0.5;
-  // fade += fadeAmnt;
 
-  if (ply == true) {
-    rain.display();
+  if (song.currentTime() >= 1 && song.currentTime() <= 4) {
+    image(hw, 0, 0, windowWidth, windowHeight);
+    scale(0.4);
+    animation(rain, windowWidth, windowHeight);
   }
 
-}
+  if (song.currentTime() > 4 && song.currentTime() <= 7) {
+    image(stars, 0, 0, windowWidth, windowHeight);
+    scale(0.4);
+    animation(girl, windowWidth / 2 + 1100, windowHeight / 2 + 510);
+  }
+
+  if (song.currentTime() > 7 && song.currentTime() <= 12) {
+    scale(0.5);
+    animation(heart, windowWidth / 2 + 700, windowHeight / 2 + 400);
+  }
+
+  if (song.currentTime() > 12 && song.currentTime() <= 17) {
+    scale(0.5);
+    animation(rose, windowWidth / 2 + 700, windowHeight / 2 + 400);
+  }
+
+} //end of draw
